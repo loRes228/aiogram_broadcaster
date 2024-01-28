fb9000bddfe06cb8edcca2a4eb10bf2a86ad6719
loRes <61621244+loRes228@users.noreply.github.com>
2024-01-28 22:12:40 +0200

    delete types

--- aiogram_broadcaster/types_.py
+++
@@ -1,6 +0,0 @@
-from datetime import timedelta
-from typing import Iterable, Sequence, Union
-
-
-ChatsIds = Union[Iterable[Union[int, str]], Sequence[Union[int, str]]]
-Interval = Union[float, int, timedelta]
