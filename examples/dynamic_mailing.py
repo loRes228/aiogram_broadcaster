2293a91b7338de88766e389bad4f3639de6d3fbb
loRes <61621244+loRes228@users.noreply.github.com>
2024-02-11 15:19:40 +0200

    update examples

--- examples/dynamic_mailing.py
+++ examples/dynamic_mailing.py
@@ -8,13 +8,14 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import Message
 
-from aiogram_broadcaster import Broadcaster
+from aiogram_broadcaster import Broadcaster, skip_event
 from aiogram_broadcaster.mailer import Mailer
 from aiogram_broadcaster.storage.redis import RedisMailerStorage
 
 
 TOKEN = "1234:Abc"  # noqa: S105
 CHATS_IDS_TO_MAILING = [61043901, 78238238, 78378343, 98765431, 12345678]
+ADDITIONAL_CHAT_IDS = [11043901, 78333383, 99372323, 12756556, 56563681]
 
 router = Router(name=__name__)
 
@@ -49,6 +50,12 @@ async def on_state_message(
     mailer.start()
 
 
+async def append_chats(mailer: Mailer) -> None:
+    if await mailer.add_chats(chat_ids=ADDITIONAL_CHAT_IDS):
+        await mailer.run()
+        skip_event()
+
+
 async def notify_complete(mailer: Mailer) -> None:
     statistic = mailer.statistic()
     await mailer.message.reply(
@@ -74,6 +81,7 @@ def main() -> None:
         storage=storage,
         auto_setup=True,
     )
+    broadcaster.event.complete.register(append_chats)
     broadcaster.event.complete.register(notify_complete)
 
     dispatcher.run_polling(bot)
