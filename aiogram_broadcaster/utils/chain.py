abd13ec6a6921ca0d9af04d925e725f0ebf0e092
loRes <61621244+loRes228@users.noreply.github.com>
2024-05-23 19:18:36 +0300

    improved chain object

--- aiogram_broadcaster/utils/chain.py
+++ aiogram_broadcaster/utils/chain.py
@@ -8,17 +8,18 @@ UNSET_ENTITY = sentinel.UNSET_ENTITY
 
 
 class ChainObject(Generic[EntityType]):
-    __chain_root__: ClassVar[bool] = False
-    __entity: EntityType = UNSET_ENTITY
-    __sub_name: ClassVar[str]
+    __chain_entity__: EntityType
+    __chain_sub_name__: ClassVar[str]
+    __chain_root__: ClassVar[bool]
     name: str
     head: Optional[EntityType]
     tail: List[EntityType]
 
     def __init_subclass__(cls, **kwargs: Any) -> None:
-        if cls.__entity is UNSET_ENTITY:
-            cls.__entity = cls
-            cls.__sub_name = kwargs.pop("sub_name", cls.__name__.lower())
+        if not hasattr(cls, "__chain_entity__"):
+            cls.__chain_root__ = False
+            cls.__chain_entity__ = cls
+            cls.__chain_sub_name__ = kwargs.pop("sub_name", cls.__name__.lower())
         super().__init_subclass__(**kwargs)
 
     def __init__(self, name: Optional[str] = None) -> None:
@@ -27,14 +28,16 @@ class ChainObject(Generic[EntityType]):
         self.tail = []
 
     def __repr__(self) -> str:
-        fields = [f"name={self.name!r}"]
+        fields = []
+        fields.append(f"name={self.name!r}")
         if self.tail:
             fields.append(f"nested={list(self.tail)!r}")
         fields_sting = ", ".join(fields)
         return f"{type(self).__name__}({fields_sting})"
 
     def __str__(self) -> str:
-        fields = [f"name={self.name!r}"]
+        fields = []
+        fields.append(f"name={self.name!r}")
         if self.head:
             fields.append(f"parent={self.head!s}")
         fields_sting = ", ".join(fields)
@@ -62,39 +65,34 @@ class ChainObject(Generic[EntityType]):
     def include(self: EntityType, *entities: EntityType) -> Optional[EntityType]:
         if not entities:
             raise ValueError(
-                f"At least one {self.__sub_name} must be provided to include.",
+                f"At least one {self.__chain_sub_name__} must be provided to include.",
             )
         for entity in entities:
-            if not isinstance(entity, self.__entity):
+            if not isinstance(entity, self.__chain_entity__):
                 raise TypeError(
-                    f"The {self.__sub_name} must be an instance of "
-                    f"{self.__entity.__name__}, not a {type(entity).__name__}.",
+                    f"The {self.__chain_sub_name__} must be an instance of "
+                    f"{self.__chain_entity__.__name__}, not a {type(entity).__name__}.",
                 )
             entity._chain_bind(entity=self)  # noqa: SLF001
         return entities[-1] if len(entities) == 1 else None
 
     def _chain_bind(self: EntityType, entity: EntityType) -> None:
-        if self.__chain_root__:
-            raise RuntimeError(
-                f"{type(self).__name__} cannot be attached to another {self.__sub_name}.",
-            )
-        if not isinstance(entity, self.__entity):
-            raise TypeError(
-                f"The {self.__sub_name} must be an instance of "
-                f"{self.__entity.__name__}, not a {type(entity).__name__}.",
+        if self == entity:
+            raise ValueError(
+                f"Cannot include the {self.__chain_sub_name__} on itself.",
             )
         if self.head:
             raise RuntimeError(
-                f"The {self.__sub_name} name={self.name!r} is already attached to "
-                f"{self.__sub_name} name={self.head.name!r}.",
-            )
-        if self == entity:
-            raise ValueError(
-                f"Cannot include the {self.__sub_name} on itself.",
+                f"The {self.__chain_entity__.__name__}(name={self.name!r}) is already attached to "
+                f"{self.__chain_entity__.__name__}(name={self.head.name!r}).",
             )
         if self in entity.chain_head:
             raise RuntimeError(
                 "Circular referencing detected.",
+            )
+        if self.__chain_root__:
+            raise RuntimeError(
+                f"{type(self).__name__} cannot be attached to another {self.__chain_sub_name__}.",
             )
         self.head = entity
         entity.tail.append(self)
