e78a18a0bf03170147690893035c4babfc392fc7
loRes <61621244+loRes228@users.noreply.github.com>
2024-05-31 21:12:21 +0300

    Improved MailerGroup

--- aiogram_broadcaster/mailer/group.py
+++ aiogram_broadcaster/mailer/group.py
@@ -1,4 +1,4 @@
-from asyncio import create_task, gather, wait
+from asyncio import ensure_future, gather, wait
 from typing import Any, Coroutine, Dict, Iterable, Optional, Set, Union
 
 from .container import MailerContainer
@@ -8,7 +8,7 @@ from .mailer import Mailer
 class MailerGroup(MailerContainer):
     def start(self) -> Dict[Mailer, Optional[Exception]]:
         results: Dict[Mailer, Optional[Exception]] = {}
-        for mailer in self._mailers.values():
+        for mailer in self:
             try:
                 mailer.start()
                 results[mailer] = None
@@ -17,36 +17,33 @@ class MailerGroup(MailerContainer):
         return results
 
     async def wait(self) -> None:
-        futures = [mailer.wait() for mailer in self._mailers.values()]
-        await wait(futures)
+        if not self._mailers:
+            return
+        await wait(ensure_future(mailer.wait()) for mailer in self)
 
     async def run(self) -> Dict[Mailer, Union[Exception, bool]]:
-        futures = [mailer.run() for mailer in self._mailers.values()]
-        return await self._gather_futures(*futures)
+        return await self._gather_targets(mailer.run() for mailer in self)
 
     async def stop(self) -> Dict[Mailer, Optional[Exception]]:
-        futures = [mailer.stop() for mailer in self._mailers.values()]
-        return await self._gather_futures(*futures)
+        return await self._gather_targets(mailer.stop() for mailer in self)
 
     async def destroy(self) -> Dict[Mailer, Optional[Exception]]:
-        futures = [mailer.destroy() for mailer in self._mailers.values()]
-        return await self._gather_futures(*futures)
+        return await self._gather_targets(mailer.destroy() for mailer in self)
 
     async def add_chats(self, chats: Iterable[int]) -> Dict[Mailer, Union[Exception, Set[int]]]:
-        futures = [mailer.add_chats(chats=chats) for mailer in self._mailers.values()]
-        return await self._gather_futures(*futures)
+        return await self._gather_targets(mailer.add_chats(chats=chats) for mailer in self)
 
     async def reset_chats(self) -> Dict[Mailer, Union[Exception, bool]]:
-        futures = [mailer.reset_chats() for mailer in self._mailers.values()]
-        return await self._gather_futures(*futures)
+        return await self._gather_targets(mailer.reset_chats() for mailer in self)
 
     async def send(self, chat_id: int) -> Dict[Mailer, Any]:
-        futures = [mailer.send(chat_id=chat_id) for mailer in self._mailers.values()]
-        return await self._gather_futures(*futures)
+        return await self._gather_targets(mailer.send(chat_id=chat_id) for mailer in self)
 
-    async def _gather_futures(self, *futures: Coroutine[Any, Any, Any]) -> Dict[Mailer, Any]:
-        if not futures:
+    async def _gather_targets(
+        self,
+        targets: Iterable[Coroutine[Any, Any, Any]],
+    ) -> Dict[Mailer, Any]:
+        if not targets:
             return {}
-        tasks = [create_task(future) for future in futures]
-        results = await gather(*tasks, return_exceptions=True)
-        return dict(zip(self._mailers.values(), results))
+        results = await gather(*targets, return_exceptions=True)
+        return dict(zip(self, results))
